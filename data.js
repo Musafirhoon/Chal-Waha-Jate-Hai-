98c3ca681c4bd2ee4207493f8f1a22aca1e104f5
Musafirhoon <mrsavage9616@gmail.com>
2026-09-16 15:15:12 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,26 @@
+const batches = [
+  {
+    id: "batch-1",
+    name: "🎯 KILO BATCH",
+    target: "CDS 1 2027 Target",
 
+    subjects: [
+      {
+        id: "english",
+        name: "🇬🇧 English"
+      },
+      {
+        id: "gs",
+        name: "🌍 GS"
+      },
+      {
+        id: "math",
+        name: "➗ Math"
+      },
+      {
+        id: "extra-material",
+        name: "📦 Extra Material"
+      }
+    ]
+  }
+];
