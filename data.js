891274d40530bc09203836e2f882ecd35c64b432
Musafirhoon <mrsavage9616@gmail.com>
2026-09-16 15:56:08 +0530

    Update data.js

--- data.js
+++ data.js
@@ -7,16 +7,67 @@ const batches = [
     subjects: [
       {
         id: "english",
-        name: "🇬🇧 English"
+        name: "🇬🇧 English",
+        page: "english"
       },
+
       {
         id: "gs",
-        name: "🌍 GS"
+        name: "🌍 GS",
+
+        chapters: [
+          {
+            id: "ancient-history",
+            name: "🏛️ Ancient History"
+          },
+          {
+            id: "medieval-history",
+            name: "🏰 Medieval History"
+          },
+          {
+            id: "modern-history",
+            name: "🇮🇳 Modern History"
+          },
+          {
+            id: "physical-geography",
+            name: "🌍 Physical Geography"
+          },
+          {
+            id: "indian-geography",
+            name: "🗺️ Indian Geography"
+          },
+          {
+            id: "physics",
+            name: "⚛️ Physics"
+          },
+          {
+            id: "chemistry",
+            name: "🧪 Chemistry"
+          },
+          {
+            id: "biology",
+            name: "🧬 Biology"
+          },
+          {
+            id: "economics",
+            name: "💰 Economics"
+          },
+          {
+            id: "polity",
+            name: "⚖️ Polity"
+          },
+          {
+            id: "one-pager",
+            name: "📄 One Pager"
+          }
+        ]
       },
+
       {
         id: "math",
         name: "➗ Math"
       },
+
       {
         id: "extra-material",
         name: "📦 Extra Material"
