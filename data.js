7308e87b8a1f4d68ee853f3428a22b32b78c9090
Musafirhoon <mrsavage9616@gmail.com>
2026-09-16 16:44:23 +0530

    Update data.js

--- data.js
+++ data.js
@@ -17,77 +17,88 @@ const batches = [
 
         chapters: [
           {
-  id: "ancient-history",
-  name: "🏛️ Ancient History",
+            id: "ancient-history",
+            name: "🏛️ Ancient History",
 
-  chapters: [
-    {
-  id: "lecture-1",
-  name: "Lecture 1",
-  video: "1nudGgkObV7FIUtQWm_HxT75KhUKHYLJC"
-},
+            chapters: [
+              {
+                id: "chapter-1",
+                name: "Chapter 1",
 
-  lectures: [
-    {
-      id: "lecture-1",
-      name: "Lecture 1"
-    },
-    {
-      id: "lecture-2",
-      name: "Lecture 2"
-    },
-    {
-      id: "lecture-3",
-      name: "Lecture 3"
-    }
-  ]
-},
-    {
-      id: "chapter-2",
-      name: "Chapter 2"
-    },
-    {
-      id: "chapter-3",
-      name: "Chapter 3"
-    }
-  ]
-},
+                lectures: [
+                  {
+                    id: "lecture-1",
+                    name: "Lecture 1",
+                    video: "1nudGgkObV7FIUtQWm_HxT75KhUKHYLJC"
+                  },
+                  {
+                    id: "lecture-2",
+                    name: "Lecture 2"
+                  },
+                  {
+                    id: "lecture-3",
+                    name: "Lecture 3"
+                  }
+                ]
+              },
+
+              {
+                id: "chapter-2",
+                name: "Chapter 2"
+              },
+
+              {
+                id: "chapter-3",
+                name: "Chapter 3"
+              }
+            ]
+          },
+
           {
             id: "medieval-history",
             name: "🏰 Medieval History"
           },
+
           {
             id: "modern-history",
             name: "🇮🇳 Modern History"
           },
+
           {
             id: "physical-geography",
             name: "🌍 Physical Geography"
           },
+
           {
             id: "indian-geography",
             name: "🗺️ Indian Geography"
           },
+
           {
             id: "physics",
             name: "⚛️ Physics"
           },
+
           {
             id: "chemistry",
             name: "🧪 Chemistry"
           },
+
           {
             id: "biology",
             name: "🧬 Biology"
           },
+
           {
             id: "economics",
             name: "💰 Economics"
           },
+
           {
             id: "polity",
             name: "⚖️ Polity"
           },
+
           {
             id: "one-pager",
             name: "📄 One Pager"
