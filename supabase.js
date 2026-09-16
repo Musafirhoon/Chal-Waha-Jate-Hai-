ed0c553099f36ad42dae02dda8faf8fd7403b2f9
Musafirhoon <mrsavage9616@gmail.com>
2026-09-16 21:26:43 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://ixloyvwvezfyvcswjgjf.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://ixloyvwvezfyvcswjgjf.supabase.co";
 const SUPABASE_KEY = "sb_publishable_ST75SqpIHRyBidO9UQc4xA_y7r8YCeK";
 const supabaseClient = supabase.createClient(
   SUPABASE_URL,
